6833957b82035a67b31e34f82e4b94cf673561d9
adityatiwari-legend <adityatiwariofficial2005@gmail.com>
2025-02-06 22:59:49 +0530

    Add files via upload

---
+++ try3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+
+ int main()
+ {
+    int a , b;
+    cout<< "enter the value of a ";
+    cin>>a;
+    cout<< "enter the value of b ";
+    cin>>b;
+
+    cout << "the sub of a and b is " << a+b;
+    return 0;
+ }
+ 
