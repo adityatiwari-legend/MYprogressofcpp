a654ef3f4081f1a1576b731f26ab6d3d44a83586
adityatiwari-legend <adityatiwariofficial2005@gmail.com>
2025-02-08 19:15:46 +0530

    Add files via upload
    day 2

---
+++ add-multi-sub.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a ,b;
+
+    cout<< "enter the value of a ";
+    cin>> a;
+
+    cout<<"enter the value of b ";
+    cin>>b;
+
+
+int add = a+b;
+int sub = a-b;
+int multi= a*b;
+
+cout<<"the addition of"<<a<<"and"<<b<<"is: "<<add<<endl;
+cout<<"the substraction of"<<a<<"and"<<b<<"is: "<<sub<<endl;
+cout<<"the multiply of"<<a<<"and"<<b<<"is: "<<multi<<endl;
+    return 0;
+}
